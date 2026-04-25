8f7e1faf60dd9ead604121f64ae2568de929a27a
AasthaShah2 <shahaastha2403@gmail.com>
2026-04-25 18:35:26 +0530

    base setup

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from app.routes import user_routes
+
+app = FastAPI()
+app.include_router(user_routes.router)
+
+@app.get("/")
+def root():
+    return {"message": "API is running 🚀"}
