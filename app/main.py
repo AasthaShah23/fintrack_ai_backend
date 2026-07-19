0c9deb9497853456d969ecf5fb416d13e5a42d21
AasthaShah2 <shahaastha2403@gmail.com>
2026-07-19 17:21:42 +0530

    feat: add is_delete column to transactions and update transaction model

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes.auth import routes as auth_routes
 from app.routes.categories import routes as category_routes
 from app.routes.budget import routes as budget_routes
+from app.routes.transactions import routes as transaction_routes
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -21,7 +22,7 @@ app.add_middleware(
 app.include_router(auth_routes.router)
 app.include_router(category_routes.router)
 app.include_router(budget_routes.router)
-
+app.include_router(transaction_routes.router)
 @app.get("/")
 def root():
     return {"message": "API is running 🚀"}
